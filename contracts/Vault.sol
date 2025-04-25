306b95e023151ca43560343ccd4fca679b422fc2
Dyslex7c <polynsominiac@gmail.com>
2025-04-25 18:20:11 +0530

    afterdeposit functions

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -5,11 +5,82 @@ pragma solidity ^0.8.20;
 import "./ERC4626Fees.sol";
 
 contract Vault is ERC4626Fees {
-    address payable public owner;
+    address payable public vaultOwner;
     uint256 public entryFeeBasisPoints;
 
     constructor(IERC20 _asset, uint256 _basisPoints) ERC4626(_asset) ERC20("Vault Polar Token", "vPT") {
-        owner = payable(msg.sender);
+        vaultOwner = payable(msg.sender);
         entryFeeBasisPoints = _basisPoints;
+    }
+
+    /** @dev See {IERC4626-deposit}. */
+    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
+        uint256 maxAssets = maxDeposit(receiver);
+        if (assets > maxAssets) {
+            revert ERC4626ExceededMaxDeposit(receiver, assets, maxAssets);
+        }
+
+        uint256 shares = previewDeposit(assets);
+        _deposit(_msgSender(), receiver, assets, shares);
+        afterDeposit(assets);
+
+        return shares;
+    }
+
+    /** @dev See {IERC4626-mint}. */
+    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
+        uint256 maxShares = maxMint(receiver);
+        if (shares > maxShares) {
+            revert ERC4626ExceededMaxMint(receiver, shares, maxShares);
+        }
+
+        uint256 assets = previewMint(shares);
+        _deposit(_msgSender(), receiver, assets, shares);
+        afterDeposit(assets);
+
+        return assets;
+    }
+
+    /** @dev See {IERC4626-withdraw}. */
+    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
+        uint256 maxAssets = maxWithdraw(owner);
+        if (assets > maxAssets) {
+            revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
+        }
+
+        uint256 shares = previewWithdraw(assets);
+        beforeWithdraw(assets, shares);
+        _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+        return shares;
+    }
+
+    /** @dev See {IERC4626-redeem}. */
+    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
+        uint256 maxShares = maxRedeem(owner);
+        if (shares > maxShares) {
+            revert ERC4626ExceededMaxRedeem(owner, shares, maxShares);
+        }
+
+        uint256 assets = previewRedeem(shares);
+        beforeWithdraw(assets, shares);
+        _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+        return assets;
+    }
+
+    function _entryFeeBasisPoints() internal view override returns (uint256) {
+        return entryFeeBasisPoints;
+    }
+
+    function _entryFeeRecipient() internal view override returns (address) {
+        return vaultOwner;
+    }
+
+    function beforeWithdraw(uint256 assets, uint256 shares) internal virtual {}
+
+    function afterDeposit(uint256 assets) internal virtual {
+        uint256 interest = assets / 10;
+        SafeERC20.safeTransferFrom(IERC20(asset()), vaultOwner, address(this), interest);
     }
 }
